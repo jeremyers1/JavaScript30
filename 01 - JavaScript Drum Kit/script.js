3f8f48f3717dac67b4c3d9a48f5af95273b2b7aa
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-07-06 13:52:35 -0700

    Add Yellow border on button keydown

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -11,27 +11,42 @@ https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
 
 This lesson used e.keyCode which is deprecated. 
 Furthermore, there are other more appropriate attributes
-than data-key to access the key pressed during keydown,
-such as, simply, the default e.key
+than data-key, such as, simply, e.key
+https://stackoverflow.com/questions/1846599/how-to-find-out-what-character-key-is-pressed
 
 */
 
 
-
-window.addEventListener('keydown', function (e) {
+window.addEventListener('keydown', function(e){
     console.log(e);
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}]`);
-    const yellowKey = document.querySelector(`div[e.key=$'{e.key}']`);
-    console.log(audio, e.key, `audio[${e.keyCode}]`);
+    /* removed his approach because keyCode is deprecated
+    he even complained in the video that keyCode was hard to remember
+    so the HTML data-keys have been updated as well, 
 
-    yellowKey.classList.add("playing");
+    Note: e.key returns which key was pressed, but it does not access the 
+    div or audio elements 
 
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    */
+    const audio2 = document.querySelector(`audio[data-key="${e.key}"]`);
+    const audio3 = e.key; // can't use to play audio
+    console.log(audio2, audio3);
+
+    const btnPlaying = document.querySelector(`div[data-key="${e.key}"]`);
+    const btnPlaying2 = e.key; // can't use to update classes
+    console.log(btnPlaying, btnPlaying2);
+
+    btnPlaying.classList.add("playing");
+
+    /* NOTE: for some reason, keyup has to go INSIDE 'keydown' eventListener or else it throws error */
+    window.addEventListener('keyup', function () {
+         btnPlaying.classList.remove("playing");
+    });
 });
 
-/*
-
-window.addEventListener('keyup', function (e) {
-    const yellowKey = document.querySelector(`div[${e.key}]`);
-    yellowKey.classList.remove("playing");
+/* Leaving my attempt here to see if I can fix it later?
+window.addEventListener('keyup', function(e) {
+    const btnDone = document.querySelector(`div[data-key="${e.key}"]`);
+    btnDone.classList.remove("playing");
 });
 */
