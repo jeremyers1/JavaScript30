e47c8d5a3f5e91200b18a476ec2033a733b67083
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-07-07 14:07:32 -0700

    Make minute hand move incrementally

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,27 +1,46 @@
-const secondHand = document.querySelector(".second-hand");
-const minuteHand = document.querySelector(".min-hand");
-const hourHand = document.querySelector(".hour-hand");
+/* Also was helpful: 
+https://www.youtube.com/watch?v=Ki0XXrlKlHY
+https://github.com/WebDevSimplified/JavaScript-Clock
+He uses a custom property --rotation to place numbers and hand rotation
+See https://developer.mozilla.org/en-US/docs/Web/CSS/--* 
+*/
 
 let allNum = document.querySelectorAll('.number');
 for(let num of allNum){
     num.style.transform = `rotate(${30*num.innerText}deg)`;
 }
 
-function setDate(){
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+console.log(secondHand, minuteHand, hourHand);
+
+function setClock(){
     const now = new Date();
     const seconds = now.getSeconds();
     const minutes = now.getMinutes();
     const hours = now.getHours();
-    // console.log(`${hours}:${minutes}:${seconds}`);
+//    console.log(`${hours}:${minutes}:${seconds}`);
 
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    const minutesDegrees = ((minutes / 60) * 360) + 90;
-    const hoursDegrees = ((hours / 12) * 360) + 90;
-    console.log(minutesDegrees, hoursDegrees);
+    const secondsRatio = (seconds / 60);
+    const minutesRatio = (secondsRatio + minutes) / 60;
+    const hoursRatio = (minutesRatio + hours) / 12;
+
+    const secondsDegrees = (secondsRatio * 360) + 90;
+    const minutesDegrees = (minutesRatio * 360) + 91;
+    const hoursDegrees = (hoursRatio * 360) + 90;
+
+ //   console.log(hoursDegrees, minutesDegrees, secondsDegrees);
 
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
-    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees})deg)`;
+
+    /* Alternate Method ... but more complicated
+    secondHand.style.setProperty('transform', `rotate(${secondsDegrees}deg)`);
+    minuteHand.style.setProperty('transform', `rotate(${minutesDegrees}deg)`);
+    hourHand.style.setProperty('transform', `rotate(${hoursDegrees}deg)`);
+    */
 
     if (secondsDegrees <= 90){
         secondHand.style.transition = "none";
@@ -30,5 +49,4 @@ function setDate(){
         secondHand.style["transition-timing-function"] = "cubic-bezier(0, 1.44, 0, 1.87)";
     }
 }
-
-setInterval(setDate, 1000);
+setInterval(setClock, 1000);
