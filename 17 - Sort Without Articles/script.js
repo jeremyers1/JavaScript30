dc33d92e26b7adccda68bd8f40f539bf75491bcd
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-07-20 12:09:43 -0700

    Add alphabetizing function

---
+++ 17 - Sort Without Articles/script.js
@@ -0,0 +1,29 @@
+const bands = [
+	'The Plot in You',
+	'The Devil Wears Prada',
+	'Pierce the Veil',
+	'Norma Jean',
+	'The Bled',
+	'Say Anything',
+	'The Midway State',
+	'We Came as Romans',
+	'Counterparts',
+	'Oh, Sleeper',
+	'A Skylit Drive',
+	'Anywhere But Here',
+	'An Old Dog',
+];
+
+function removeArticle(str) {
+	let words = str.split(' ');
+	if (words[0] === 'A' || words[0] === 'An' || words[0] === 'The') {
+		words.shift();
+		return words.join(' ');
+	}
+	return str;
+}
+
+const alpphaBands = bands.sort((a, b) =>
+	removeArticle(a) > removeArticle(b) ? 1 : -1
+);
+console.log(alphaBands);
